38be560e5c79fcad18b3dfbcc9a44a22ba7bb3ca
Bernard Mordan <bernardmordan@gmail.com>
2014-06-22 00:25:10 +0100

    adding my branch devs

--- leaderboard.js
+++ leaderboard.js
@@ -1,7 +1,4 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
-
-Players = new Meteor.Collection("players");
+Pupils = new Meteor.Collection("pupils");
 
 if (Meteor.isClient) {
   
@@ -12,71 +9,43 @@ if (Meteor.isClient) {
      var dd  = this.getDate().toString();
      return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]); 
     };
-
   d = new Date();
   
   
   Template.switch.events({
-
     'click' : function (e){
-      if(Session.get("page") === "spread"){
-        
-        e.delegateTarget.children[1].style.display = 'none';
-        e.delegateTarget.children[0].style.display = 'block';
-        Session.set("page", "controls");
-      }else if(Session.get("page") === "controls"){
-        e.delegateTarget.children[0].style.display = 'none';
-        e.delegateTarget.children[1].style.display = 'block';
-        Session.set("page", "spread");
+        if(Session.get("page") === "spread"){
+          e.delegateTarget.children[1].style.display = 'none';
+          e.delegateTarget.children[0].style.display = 'block';
+          Session.set("page", "controls");
+        }else if(Session.get("page") === "controls"){
+          e.delegateTarget.children[0].style.display = 'none';
+          e.delegateTarget.children[1].style.display = 'block';
+          Session.set("page", "spread");
+        }
       }
+  });
+  
+  Template.add.events({
+    'click' : function(e){
+      var name = e.delegateTarget.children['name'].value;
+      var age = e.delegateTarget.children['age'].value;
+      var bed = e.delegateTarget.children['bed'].value;
+      var list = e.delegateTarget.children['list'].value;
+      var ward = e.delegateTarget.children['ward'].value;
+      Pupils.insert({date: d.yyyymmdd(), name: name, age: age, bed: bed, list: list, ward: ward});
     }
   });
   
-  Template.name.events({
-    'blur' : function(e){
-      console.log(e.target.value);
-      console.log(d.yyyymmdd());
-    }
-  });
-  
-  Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
+  Template.early.list = function(){
+    return Pupils.find({list: 'E'});
   };
 
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
-
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
-
-  Template.leaderboard.events({
-    'click input.inc': function (e) {
-      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
-
-  Template.player.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  });
 }
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
-    }
+    
   });
 }
